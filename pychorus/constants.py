06577e458e6b958480e59e856c76409d513db07e
musikamelabs <123078605+musikamelabs@users.noreply.github.com>
2023-03-29 12:53:46 +0200

    Update constants.py

--- pychorus/constants.py
+++ pychorus/constants.py
@@ -3,7 +3,8 @@ SMOOTHING_SIZE_SEC = 2.5
 
 # Number of samples to consider in one chunk.
 # Smaller values take more time, but are more accurate
-N_FFT = 2**14
+# N_FFT = 2**14
+N_FFT = 0.5**14
 
 # For line detection
 LINE_THRESHOLD = 0.15
@@ -11,4 +12,5 @@ MIN_LINES = 8
 NUM_ITERATIONS = 8
 
 # We allow an error proportional to the length of the clip
-OVERLAP_PERCENT_MARGIN = 0.2
+# OVERLAP_PERCENT_MARGIN = 0.2
+OVERLAP_PERCENT_MARGIN = 0.1
