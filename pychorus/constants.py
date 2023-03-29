2e604862ec9e1450611027a1f02058f6d5755baa
musikamelabs <123078605+musikamelabs@users.noreply.github.com>
2023-03-29 13:11:34 +0200

    Update constants.py

--- pychorus/constants.py
+++ pychorus/constants.py
@@ -8,8 +8,8 @@ N_FFT = 4**14
 
 # For line detection
 LINE_THRESHOLD = 0.15
-MIN_LINES = 8
-NUM_ITERATIONS = 8
+MIN_LINES = 4
+NUM_ITERATIONS = 4
 
 # We allow an error proportional to the length of the clip
 OVERLAP_PERCENT_MARGIN = 0.5
